a096429c7e68707effdba8a2cad20a036512e835
Mathilde4 <mathibassadou@gmail.com>
2025-02-26 12:36:20 +0000

    ajout du dossier image

--- app/main.py
+++ app/main.py
@@ -6,6 +6,9 @@ from app.routes.admin_routes import app_admin
 from app.routes.user_routes import app_user
 from app.routes.categorie_routes import app_categorie
 from app.routes.produit_routes import app_produit
+from fastapi import FastAPI, HTTPException
+from fastapi.responses import FileResponse
+import os
 app = FastAPI()
 app.include_router(app_categorie)
 app.include_router(app_produit)
@@ -17,17 +20,34 @@ app.include_router(app_admin)
 origins = [
     "http://localhost:5173",  # React en développement
     "http://127.0.0.1:5173",
+    "http://localhost:8000",  # FastAPI Local
+    "http://127.0.0.1:8000",
+     "*",  # Autoriser temporairement toutes les origines (à modifier en prod)  
 ]
 
 # Ajout du middleware CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,  # Autoriser ces origines
+    allow_origins=origins,  # Autoriser ces origines 
     allow_credentials=True,
     allow_methods=["*"],  # Autoriser toutes les méthodes (GET, POST, PUT, DELETE, etc.)
     allow_headers=["*"],  # Autoriser tous les headers
 )
 
+UPLOAD_DIR = "images"
+os.makedirs(UPLOAD_DIR, exist_ok=True)
+
 @app.get("/")
 async def hello():
     return {"message": "Hello, World!"}
+
+# Endpoint pour récupérer une image
+@app.get("/images/{filename}")
+async def get_image(filename: str):
+    file_path = os.path.join(UPLOAD_DIR, filename)
+
+    # Vérifier si l’image existe
+    if not os.path.exists(file_path):
+        raise HTTPException(status_code=404, detail="Image not found")
+
+    return FileResponse(file_path)
